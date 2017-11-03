e5057ac510fb2d6ebdea73da6570689ded59f376
965993 <yogeesh222@gmail.com>
2017-11-03 19:55:06 +0530

    implementing CRUD operations

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 var app = angular.module("myShoppingList", []); 
-app.controller("myCtrl", function($scope,$interval) {
+app.controller("myCtrl", function($scope,$interval,$http) {
     $scope.teammates = ["yogeesh", "sathish", "anusha","divyasingh","divyapothu"];
    $scope.theTime = new Date().toLocaleTimeString();
   $interval(function () {
       $scope.theTime = new Date().toLocaleTimeString();
   }, 1000);
-});
+  $scope.add = function(){
+  	     if ($scope.name != '' && angular.isDefined($scope.name))
+  	     {
+  	     	 // ADD A NEW ELEMENT.
+                    $scope.teammates.push($scope.name);
+
+                    // CLEAR THE FIELDS.
+                    $scope.name = '';
+  	     	
+  	     } 
+  	     else
+                {
+                   alert('please enter the name');
+                }
+  }
+  $scope.get = function(){
+  	$http.get('http://rest-service.guides.spring.io/greeting').
+        then(function(response) {
+            $scope.greeting = response.data;
+        });
+    }
+  
+});	
